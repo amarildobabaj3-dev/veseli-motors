4148d45e3b748155b2a6f92e5230593edb6bff01
amarildobabaj3-dev <amarildobabaj3@gmail.com>
2026-02-27 02:52:35 +0100

    Fix final per Sign Up

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
-app.secret_key = 'veseli_motors_premium_key'
+app.secret_key = 'veseli_motors_final_fix'
 
-# Konfigurimi i Databazës
+# Konfigurimi i Databazës (E thjeshtuar maksimalisht)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'veseli_final.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'motors.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -21,8 +20,8 @@ class User(db.Model):
 # Modeli i Makinës
 class Makina(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    marka = db.Column(db.String(50), nullable=False)
-    modeli = db.Column(db.String(50), nullable=False)
+    marka = db.Column(db.String(50))
+    modeli = db.Column(db.String(50))
     viti = db.Column(db.Integer)
     cmimi = db.Column(db.Float)
     karburanti = db.Column(db.String(20))
@@ -32,6 +31,7 @@ class Makina(db.Model):
     foto_2 = db.Column(db.String(500))
     pershkrimi = db.Column(db.Text)
 
+# Kjo komande krijon databazen automatikisht
 with app.app_context():
     db.create_all()
 
@@ -39,47 +39,44 @@ with app.app_context():
 def home():
     return render_template('home.html')
 
-@app.route('/salloni')
-def salloni():
-    makinat = Makina.query.all()
-    return render_template('salloni.html', makinat=makinat)
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Krijon llogarine e re ne databaze
-        hashed_pw = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
-        new_user = User(username=request.form['username'], password=hashed_pw)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+        user = request.form.get('username')
+        pw = request.form.get('password')
+        if user and pw:
+            new_user = User(username=user, password=pw)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and check_password_hash(user.password, request.form['password']):
+        user = User.query.filter_by(username=request.form.get('username')).first()
+        if user and user.password == request.form.get('password'):
             session['user_id'] = user.id
-            # KETU ESHTE NDRYSHIMI: Emri i pronarit eshte 'pronari'
             if user.username == 'pronari':
                 session['is_admin'] = True
             return redirect(url_for('home'))
     return render_template('login.html')
 
+@app.route('/salloni')
+def salloni():
+    makinat = Makina.query.all()
+    return render_template('salloni.html', makinat=makinat)
+
 @app.route('/shto', methods=['GET', 'POST'])
 def shto():
-    # Vetem 'pronari' mund te shtoje makina
-    if not session.get('is_admin'): 
-        return "Nuk ke leje! Vetem Pronari mund te shtoje makina.", 403
-    
+    if not session.get('is_admin'): return "Nuk ke leje!", 403
     if request.method == 'POST':
         m = Makina(
-            marka=request.form['marka'], modeli=request.form['modeli'],
-            viti=request.form['viti'], cmimi=request.form['cmimi'],
-            karburanti=request.form['karburanti'], kambio=request.form['kambio'],
-            kilometrat=request.form['kilometrat'], foto_url=request.form['foto'],
-            foto_2=request.form['foto2'], pershkrimi=request.form['pershkrimi']
+            marka=request.form.get('marka'), modeli=request.form.get('modeli'),
+            viti=request.form.get('viti'), cmimi=request.form.get('cmimi'),
+            karburanti=request.form.get('karburanti'), kambio=request.form.get('kambio'),
+            kilometrat=request.form.get('kilometrat'), foto_url=request.form.get('foto'),
+            foto_2=request.form.get('foto2'), pershkrimi=request.form.get('pershkrimi')
         )
         db.session.add(m)
         db.session.commit()

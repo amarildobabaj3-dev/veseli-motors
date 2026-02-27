9c85ec6e641f81987a0a10093bce58aee4420a64
amarildobabaj3-dev <amarildobabaj3@gmail.com>
2026-02-27 16:03:39 +0100

    Rregullimi i fundit i trurit

--- app.py
+++ app.py
@@ -1,128 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.secret_key = 'veseli_motors_master_final_2026'
 
-# --- LIDHJA ME DATABASE-N E PERHERSHME (POSTGRES) ---
-# Kjo rresht merr linkun qe vendose te Environment Variables
-db_url = os.environ.get('DATABASE_URL')
+# LIDHJA ME DATABASE-N - MOS E PREK
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+if app.config['SQLALCHEMY_DATABASE_URI'] and app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://", 1)
 
-# Rregullim teknik per Render qe te njohe Postgres-in sakte
-if db_url and db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///motors_final.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# ---------------------------------------------------------
 
-# MODELI I PERDORUESVE
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-# MODELI I MAKINAVE
+# MODELI I MAKINËS - SHTUAM VETËM CELULARIN QË TË MOS DALË ERROR
 class Makina(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     marka = db.Column(db.String(50))
     modeli = db.Column(db.String(50))
     viti = db.Column(db.Integer)
-    cmimi = db.Column(db.Float)
+    cmimi = db.Column(db.Integer)
     karburanti = db.Column(db.String(20))
     kambio = db.Column(db.String(20))
-    kilometrat = db.Column(db.Integer)
+    kilometrat = db.Column(db.String(50))
+    celulari = db.Column(db.String(20)) # Kjo rregullon errorin 500
     foto1 = db.Column(db.Text)
     foto2 = db.Column(db.Text)
     foto3 = db.Column(db.Text)
     foto4 = db.Column(db.Text)
-    celulari = db.Column(db.String(20))
     pershkrimi = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-# KRIJIMI I TABELAVE DHE ADMINIT AUTOMATIK
-with app.app_context():
-    db.create_all()
-    # Krijon llogarine 'pronari' nese nuk eshte ne database-n e re
-    admin = User.query.filter_by(username='pronari').first()
-    if not admin:
-        new_admin = User(username='pronari', password='saadi123')
-        db.session.add(new_admin)
-        db.session.commit()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        user = request.form.get('username')
-        pw = request.form.get('password')
-        if not User.query.filter_by(username=user).first():
-            new_user = User(username=user, password=pw)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('login'))
-    return render_template('signup.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = User.query.filter_by(username=request.form.get('username')).first()
-        if user and user.password == request.form.get('password'):
-            session['user_id'] = user.id
-            session['username'] = user.username
-            return redirect(url_for('index'))
-    return render_template('login.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/salloni')
 def salloni():
-    query = request.args.get('search')
-    if query:
-        makinat = Makina.query.filter(Makina.marka.contains(query)).order_by(Makina.viti.desc()).all()
-    else:
-        makinat = Makina.query.order_by(Makina.viti.desc()).all()
+    makinat = Makina.query.all()
     return render_template('salloni.html', makinat=makinat)
 
-@app.route('/detaje/<int:id>')
-def detaje(id):
-    makina = Makina.query.get_or_404(id)
-    return render_template('detajet.html', makina=makina)
-
 @app.route('/shto', methods=['GET', 'POST'])
-def shto():
-    if 'user_id' not in session: return redirect(url_for('login'))
+def shto_makine():
     if request.method == 'POST':
-        m = Makina(
-            marka=request.form.get('marka'), modeli=request.form.get('modeli'),
-            viti=request.form.get('viti'), cmimi=request.form.get('cmimi'),
-            karburanti=request.form.get('karburanti'), kambio=request.form.get('kambio'),
-            kilometrat=request.form.get('kilometrat'), celulari=request.form.get('celulari'),
-            foto1=request.form.get('foto1'), foto2=request.form.get('foto2'),
-            foto3=request.form.get('foto3'), foto4=request.form.get('foto4'),
-            pershkrimi=request.form.get('pershkrimi'), user_id=session['user_id']
+        reja = Makina(
+            marka=request.form['marka'],
+            modeli=request.form['modeli'],
+            viti=request.form['viti'],
+            cmimi=request.form['cmimi'],
+            karburanti=request.form['karburanti'],
+            kambio=request.form['kambio'],
+            kilometrat=request.form['kilometrat'],
+            celulari=request.form['celulari'], # Ky rresht duhet patjetër
+            foto1=request.form['foto1'],
+            foto2=request.form.get('foto2', ''),
+            foto3=request.form.get('foto3', ''),
+            foto4=request.form.get('foto4', ''),
+            pershkrimi=request.form['pershkrimi']
         )
-        db.session.add(m)
+        db.session.add(reja)
         db.session.commit()
         return redirect(url_for('salloni'))
     return render_template('shto_makine.html')
 
-@app.route('/fshij/<int:id>')
-def fshij(id):
-    m = Makina.query.get(id)
-    if m and (session.get('username') == 'pronari' or m.user_id == session.get('user_id')):
-        db.session.delete(m)
-        db.session.commit()
-    return redirect(url_for('salloni'))
+@app.route('/makina/<int:id>')
+def detajet(id):
+    makina = Makina.query.get_or_4_0_4(id)
+    return render_template('detajet.html', makina=makina)
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)

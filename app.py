6d21e38bb906df68718327824c603b5fbfd95de0
amarildobabaj3-dev <amarildobabaj3@gmail.com>
2026-02-27 01:34:18 +0100

    Reset total dhe rregullim i fotove

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.secret_key = 'veseli_motors_sekret'
+app.secret_key = 'veseli_motors_key'
 
 # Konfigurimi i Database
-basedir = os.path.abspath(os.path.dirname(__name__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'veseli_final.db')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'veseli_motors.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -20,27 +20,25 @@ class Makina(db.Model):
     viti = db.Column(db.Integer, nullable=False)
     cmimi = db.Column(db.Float, nullable=False)
 
-# Krijimi i Database automatikisht
+# Krijimi i DB automatikisht
 with app.app_context():
     db.create_all()
 
-# Faqja Kryesore (Salloni)
 @app.route('/')
 def index():
     makinat = Makina.query.all()
     return render_template('index.html', makinat=makinat)
 
-# Faqja per te shtuar makina
 @app.route('/shto', methods=['GET', 'POST'])
 def shto_makine():
     if request.method == 'POST':
-        m = Makina(
-            marka=request.form['marka'],
-            modeli=request.form['modeli'],
-            viti=request.form['viti'],
-            cmimi=request.form['cmimi']
-        )
-        db.session.add(m)
+        re_marka = request.form.get('marka')
+        re_modeli = request.form.get('modeli')
+        re_viti = request.form.get('viti')
+        re_cmimi = request.form.get('cmimi')
+        
+        re_makina = Makina(marka=re_marka, modeli=re_modeli, viti=re_viti, cmimi=re_cmimi)
+        db.session.add(re_makina)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('shto_makine.html')
